0f520fb41d449fa360c84676f4c88cf3ba0adae2
Joshua Espinoza <josh6669@gmail.com>
2022-04-07 21:34:26 -0400

    test

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+let history = require("connect-history-api-fallback");
 app = express();
+app.use(history());
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 3000;
 app.listen(port);
-app.get("/*", (req, res) => res.redirect("/"));
+/* app.get("/*", (req, res) => res.redirect("/index.html"));*/
+console.log("servidor corriendo en: ", port);
